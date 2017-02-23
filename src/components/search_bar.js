a2f60296e3be0eb26b0be07a9d6abecd3012650b
Jordy Webb <jordywebb@Jordys-MacBook.local>
2017-02-23 15:03:59 -0800

    add state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 
 
-class SearchBar extends React.Component {
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+  }
   render() {
-    return <input onChange={(event) => console.log(event.target.value)} />;
+    return (
+      <div>
+        <input onChange={event => this.setState({ term: event.target.value })} />;
+      </div>
+    );
   }
 }
 
