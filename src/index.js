fc7ea48de41c2958d8471bc94b2eaead7222e7f7
Jordy Webb <jordywebb@Jordys-MacBook.local>
2017-02-24 17:42:22 -0800

    add iframe

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyDoBwOIRgPfUjtxlcUJ5y4NnUczmUOwoWM';
 
@@ -15,7 +16,7 @@ class App extends Component {
 
     this.state = { videos: [] };
 
-    YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+    YTSearch({ key: API_KEY, term: 'My little pony' }, (videos) => {
       this.setState({ videos });
     });
   }
@@ -24,6 +25,7 @@ class App extends Component {
     return (
   <div>
     <SearchBar />
+    <VideoDetail video={this.state.videos[0]} />
     <VideoList videos={this.state.videos} />
   </div>
   );
