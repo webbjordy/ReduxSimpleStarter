29ea67cbf0a2dc43bd9b491cade5f7e578f901a6
Jordy Webb <jordywebb@Jordys-MacBook.local>
2017-02-25 04:45:00 -0800

    fix typo

--- src/index.js
+++ src/index.js
@@ -14,7 +14,10 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
     YTSearch({ key: API_KEY, term: 'My little pony' }, (videos) => {
       this.setState({ videos });
@@ -25,7 +28,7 @@ class App extends Component {
     return (
   <div>
     <SearchBar />
-    <VideoDetail video={this.state.videos[0]} />
+    <VideoDetail video={this.state.selectedVideo} />
     <VideoList videos={this.state.videos} />
   </div>
   );
